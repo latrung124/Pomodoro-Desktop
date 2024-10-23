0f06c9653a9361d31f763b0988b741c18dc5c2c7
latrung124 <53443778+latrung124@users.noreply.github.com>
2024-10-24 01:07:17 +0700

    Init Gateway service

---
+++ Service/GatewayService/interface/IGatewayService.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2024 La Trung
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * This file is part of a gateway library that use for transport
+ * messages between client and server.
+ *
+ * Author: La Trung
+ */
+
+#ifndef IGATEWAYSERVICE_H_
+#define IGATEWAYSERVICE_H_
+
+class IGatewayService {
+public:
+    virtual ~IGatewayService() = default;
+
+    virtual void start() = 0;
+    virtual void stop() = 0;
+};
+
+#endif // IGATEWAYSERVICE_H_
+
